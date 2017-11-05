6b1ef7e599384a95dc424180cc5f97261f9cf031
Pete Waters <pete.d.waters@gmail.com>
2017-11-05 11:56:44 +0000

    Added tests for hourly, hourlyAt, daily, dailyAt and weekday frequencies

--- tests/Unit/FrequenciesTest.php
+++ tests/Unit/FrequenciesTest.php
@@ -89,6 +89,150 @@ class FrequenciesTest extends TestCase
         $this->assertEquals($frequencies->expression, '*/30 * * * *');
     }
 
+    /** @test */
+    public function can_set_hourly()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->hourly();
+
+        $this->assertEquals($frequencies->expression, '1 * * * *');
+    }
+
+    /** @test */
+    public function can_set_hourly_at()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->hourlyAt(30);
+
+        $this->assertEquals($frequencies->expression, '30 * * * *');
+    }
+
+    /** @test */
+    public function can_set_daily()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->daily();
+
+        $this->assertEquals($frequencies->expression, '0 0 * * *');
+    }
+
+    /** @test */
+    public function can_set_daily_at()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->dailyAt(9, 0);
+
+        $this->assertEquals($frequencies->expression, '0 9 * * *');
+    }
+
+    /** @test */
+    public function can_set_daily_at_using_defaults()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->dailyAt();
+
+        $this->assertEquals($frequencies->expression, '0 0 * * *');
+    }
+
+    /** @test */
+    public function can_set_twice_daily()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->twiceDaily(1, 12);
+
+        $this->assertEquals($frequencies->expression, '0 1,12 * * *');
+    }
+
+    /** @test */
+    public function can_set_twice_daily_using_defaults()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->twiceDaily();
+
+        $this->assertEquals($frequencies->expression, '0 1,12 * * *');
+    }
+
+    /** @test */
+    public function can_set_days()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->days(1, 3, 5);
+
+        $this->assertEquals($frequencies->expression, '* * * * 1,3,5');
+    }
+
+     /** @test */
+     public function can_set_days_using_defaults()
+     {
+         $frequencies = $this->frequencies();
+         $frequencies->days();
+ 
+         $this->assertEquals($frequencies->expression, '* * * * *');
+     }
+
+     /** @test */
+    public function can_set_mondays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->mondays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 1');
+    }
+
+     /** @test */
+     public function can_set_tuesdays()
+     {
+         $frequencies = $this->frequencies();
+         $frequencies->tuesdays();
+ 
+         $this->assertEquals($frequencies->expression, '* * * * 2');
+     }
+
+      /** @test */
+    public function can_set_wednesdays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->wednesdays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 3');
+    }
+
+     /** @test */
+     public function can_set_thursdays()
+     {
+         $frequencies = $this->frequencies();
+         $frequencies->thursdays();
+ 
+         $this->assertEquals($frequencies->expression, '* * * * 4');
+     }
+
+      /** @test */
+    public function can_set_fridays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->fridays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 5');
+    }
+
+     /** @test */
+     public function can_set_saturdays()
+     {
+         $frequencies = $this->frequencies();
+         $frequencies->saturdays();
+ 
+         $this->assertEquals($frequencies->expression, '* * * * 6');
+     }
+
+      /** @test */
+    public function can_set_sundays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->sundays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 7');
+    }
+
     protected function frequencies()
     {
         $frequencies = $this->getMockForTrait(FrequenciesTrait::class);
