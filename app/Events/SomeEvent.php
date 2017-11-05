9f229c333847b40b0cff3e7648e22695b5720d08
Pete Waters <pete.d.waters@gmail.com>
2017-11-05 13:43:37 +0000

    Added docblocks to dummy event class. Added dummy events to scheduler for testing

--- app/Events/SomeEvent.php
+++ app/Events/SomeEvent.php
@@ -6,8 +6,13 @@ use TaskHamster\Scheduler\Event;
 
 class SomeEvent extends Event
 {
+    /**
+     * Handles the event
+     *
+     * @return void
+     */
     public function handle()
     {
-        var_dump('works');
+        var_dump($this->expression);
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Carbon\Carbon;
+
+use TaskHamster\Scheduler\Kernel;
+use TaskHamster\Events\SomeEvent;
+
+$kernel = new Kernel;
+$kernel->setDate(Carbon::now()->tz('Europe/London'));
+
+/**
+ * Scheduled tasks
+ */
+$kernel->add(new SomeEvent)->daily()->at(9, 0);
+
+$kernel->run();
