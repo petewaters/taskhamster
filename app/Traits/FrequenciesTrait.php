94f619dc116792e2be93aa7eea5ab47c49fe84b1
Pete Waters <pete.d.waters@gmail.com>
2017-11-05 11:00:57 +0000

    Added insertIntoExpression method for replacing values in cron expression. Updated other methods to return instance for method chaining

--- app/Traits/FrequenciesTrait.php
+++ app/Traits/FrequenciesTrait.php
@@ -7,10 +7,45 @@ trait FrequenciesTrait
     public function cron(string $expression)
     {
         $this->expression = $expression;
+
+        return $this;
     }
 
     public function everyMinute()
     {
+        $this->expression = '* * * * *';
+
+        return $this;
+    }
+
+    public function everyTenMinutes()
+    {
+        // $this->insertIntoExpression(1, '*/10');
+        $this->expression = '*/10 * * * *';
+
+        return $this;
+    }
+
+    public function everyQuarterHour()
+    {
+        $this->expression = '*/15 * * * *';
+
+        return $this;
+    }
+
+    public function everyHalfHour()
+    {
+        $this->expression = '*/30 * * * *';
+
+        return $this;
+    }
+
+    public function insertIntoExpression(int $position, string $value)
+    {
+        $expression = explode(' ', $this->expression);
         
+        array_splice($expression, $position - 1, 1, $value);
+
+        return $this->cron(implode(' ', $expression));
     }
 }
