bf869817db2ef1ced063aced0e0abfd3c39df38a
Pete Waters <pete.d.waters@gmail.com>
2017-11-05 11:00:01 +0000

    Added tests for inserting values into cron expression at given position and chaining inserts

--- tests/Unit/FrequenciesTest.php
+++ tests/Unit/FrequenciesTest.php
@@ -17,12 +17,57 @@ class FrequenciesTest extends TestCase
     }
 
     /** @test */
+    public function can_insert_cron_expression()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->insertIntoExpression(1, '*/10');
+
+        $this->assertEquals($frequencies->expression, '*/10 * * * *');
+    }
+
+    /** @test */
+    public function can_chain_insert_cron_expressions()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->insertIntoExpression(1, '*/10')->insertIntoExpression(2, '1');
+
+        $this->assertEquals($frequencies->expression, '*/10 1 * * *');
+    }
+
+    /** @test */
     public function can_set_every_minute()
     {
         $frequencies = $this->frequencies();
         $frequencies->everyMinute();
 
         $this->assertEquals($frequencies->expression, '* * * * *');
+    }
+
+    /** @test */
+    public function can_set_every_ten_minutes()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->everyTenMinutes();
+
+        $this->assertEquals($frequencies->expression, '*/10 * * * *');
+    }
+
+    /** @test */
+    public function can_set_every_quarter_hour()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->everyQuarterHour();
+
+        $this->assertEquals($frequencies->expression, '*/15 * * * *');
+    }
+
+    /** @test */
+    public function can_set_every_half_hour()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->everyHalfHour();
+
+        $this->assertEquals($frequencies->expression, '*/30 * * * *');
     }
 
     protected function frequencies()
