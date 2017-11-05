d6f7fdf7ce24fb391537b70d9b7f956a71f4cd88
Pete Waters <pete.d.waters@gmail.com>
2017-11-05 11:14:13 +0000

    Refactored frequencies trait to use insertIntoExpression method for setting frequencies. Updated insertIntoExpression method to handle arrays and expression length.

--- app/Traits/FrequenciesTrait.php
+++ app/Traits/FrequenciesTrait.php
@@ -13,38 +13,36 @@ trait FrequenciesTrait
 
     public function everyMinute()
     {
-        $this->expression = '* * * * *';
-
-        return $this;
+        return $this->cron($this->expression);
     }
 
     public function everyTenMinutes()
     {
-        // $this->insertIntoExpression(1, '*/10');
-        $this->expression = '*/10 * * * *';
-
-        return $this;
+        return $this->insertIntoExpression(1, '*/10');
     }
 
     public function everyQuarterHour()
     {
-        $this->expression = '*/15 * * * *';
-
-        return $this;
+        return $this->insertIntoExpression(1, '*/15');
     }
 
     public function everyHalfHour()
     {
-        $this->expression = '*/30 * * * *';
-
-        return $this;
+        return $this->insertIntoExpression(1, '*/30');
     }
 
-    public function insertIntoExpression(int $position, string $value)
+    public function insertIntoExpression(int $position, $value)
     {
+        // In case of single value, cast to array so that we're always dealing with an array of values
+        $value = (array) $value;
+
         $expression = explode(' ', $this->expression);
         
+        // Insert the new value(s) at the specified position
         array_splice($expression, $position - 1, 1, $value);
+
+        // Make sure the new expression isn't longer than the allowed for an expression
+        $expression = array_slice($expression, 0, 5);
 
         return $this->cron(implode(' ', $expression));
     }
