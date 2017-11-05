21df17afe95bd37d8272df7bf52023ea7e2639ef
Pete Waters <pete.d.waters@gmail.com>
2017-11-05 10:20:21 +0000

    Added frequencies trait to Event class

--- app/Scheduler/Event.php
+++ app/Scheduler/Event.php
@@ -5,12 +5,32 @@ namespace TaskHamster\Scheduler;
 use Cron\CronExpression;
 use Carbon\Carbon;
 
+use TaskHamster\Traits\FrequenciesTrait;
+
 abstract class Event
 {
+    use FrequenciesTrait;
+
+    /**
+     * The cron expression for the event
+     *
+     * @var string
+     */
     public $expression = '* * * * *';
 
+    /**
+     * Handle the event
+     *
+     * @return void
+     */
     abstract public function handle();
 
+    /**
+     * Check if the event is due
+     *
+     * @param Carbon $date
+     * @return boolean
+     */
     public function isDueToRun(Carbon $date)
     {
         return CronExpression::factory($this->expression)->isDue($date);
